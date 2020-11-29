262460ac31b3097e4a9fc207bf188efe67e027da
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-11-29 03:28:24 +0100

    Display list of submissions

--- src/frontend/public/App.vue
+++ src/frontend/public/App.vue
@@ -1,29 +1,14 @@
 <template>
   <div id="app">
     <v-app>
-      <div id="nav">
-        HIII from Vue!
-        <div v-if="greeting">{{ greeting }}</div>
-      </div>
-      <v-container>Hello world</v-container>
-      <router-view></router-view>
+      <v-app-bar app></v-app-bar>
+      <v-main>
+        <v-container>
+          <router-view></router-view>
+        </v-container>
+      </v-main>
     </v-app>
   </div>
 </template>
 
-<script>
-import artist from 'ic:canisters/artist';
 
-export default {
-  data: () => {
-    return {
-      greeting: ''
-    }
-  },
-  created() {
-    artist.greet(window.prompt("Enter your name:")).then(greeting => {
-      this.greeting = greeting
-    });
-  }
-}
-</script>
--- src/frontend/public/views/Home.vue
+++ src/frontend/public/views/Home.vue
@@ -1,9 +1,39 @@
 <template>
   <div>
-    <v-main>
-      Hello from home!
-      <v-btn :to="{path: '/subpage'}" elevation="2">Go to subpage</v-btn>
-      <router-link to="/subpage">Go to subpage</router-link>
-    </v-main>
+    <v-list>
+      <v-list-item-group v-model="selected" active-class="pink--text">
+        <template v-for="submission in submissions">
+          <v-list-item :key="submission.id">
+            <v-list-item-content>
+              <v-list-item-title>ID: {{ submission.id }}</v-list-item-title>
+              <v-list-item-subtitle class="text--primary">{{ submission.url }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list-item-group>
+    </v-list>
+    <v-overlay :value="overlay">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </div>
 </template>
+
+<script>
+import artist from 'ic:canisters/artist';
+
+export default {
+  data: () => {
+    return {
+      submissions: [],
+      overlay: true,
+      selected: ''
+    }
+  },
+  created() {
+    artist.getAll().then(submissions => {
+      this.submissions = submissions
+      this.overlay = false
+    });
+  }
+}
+</script>
