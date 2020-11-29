77cde4f385d9b634728c9ca7972c137f684d9ffe
Alejandro Avilés <omegak@gmail.com>
2020-11-29 02:30:27 +0100

    Add fixtures script

---
+++ bin/fixtures.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+usage() {
+  progname=$(basename "$0")
+  echo "usage: ${progname}"
+}
+
+apply() {
+  dfx canister call artist add '("https://open.spotify.com/track/6HwwPLqfRbYS7peAp1OgFm?si=mYfboeKITt2yIVGxVeFCXw")'
+  dfx canister call artist add '("https://open.spotify.com/track/6UINWt8tEGPYBXEu5VehZs?si=sb0zdWgIS-iK_M0VVmBCxw")'
+}
+
+# validate argument length
+if [ $# -gt 0 ]; then
+  usage
+  exit 1
+fi
+
+apply
