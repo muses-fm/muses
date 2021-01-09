55f62401f047cc368dafeca8440f512deb58706b
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-01-09 20:33:04 +0100

    Create welcome screen
    Closes #12

--- src/frontend/public/App.vue
+++ src/frontend/public/App.vue
@@ -1,53 +1,5 @@
 <template>
-  <div id="app">
-    <v-app>
-      <v-navigation-drawer v-model="drawer" app>
-        <v-sheet color="grey lighten-4" class="pa-4">
-          <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
-          <div>john@example.com</div>
-        </v-sheet>
-
-        <v-divider />
-
-        <v-list>
-          <v-list-item v-for="{icon, title, to} in links" :key="icon" :to="to">
-            <!-- TODO: Fix font imports -->
-            <!-- <v-list-item-icon>
-              <v-icon>{{ icon }}</v-icon>
-            </v-list-item-icon> -->
-
-            <v-list-item-content>
-              <v-list-item-title>{{ title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-      <router-view v-if="!loading.enabled"></router-view>
-      <v-overlay v-else :value="loading.enabled" class="text-center">
-        <div class="mb-5">{{ loading.text }}</div>
-        <v-progress-circular indeterminate size="64"></v-progress-circular>
-      </v-overlay>
-    </v-app>
-  </div>
+  <v-app>
+    <router-view></router-view>
+  </v-app>
 </template>
-
-<script>
-import { mapState } from 'vuex'
-
-export default {
-  data: () => {
-    return {
-      drawer: null,
-      links: [
-        { title: 'My submissions', to: { name: 'Tracks' }, icon: 'list' },
-        { title: 'Submit track', to: { name: 'Submit track' }, icon: 'publish' }
-      ]
-    }
-  },
-  computed: {
-    ...mapState([
-      'loading'
-    ])
-  }
-}
-</script>
--- src/frontend/public/router/index.js
+++ src/frontend/public/router/index.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Tracks from '../views/Tracks.vue'
 import TrackSubmit from '../views/TrackSubmit.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', name: 'Home', component: Tracks },
   { path: '/track/submit', name: 'Submit track', component: TrackSubmit },
   { path: '/tracks', name: 'Tracks', component: Tracks },
+  { path: '/', name: 'Home', component: Home,},
 ]
 
 const router = new VueRouter({ routes })
