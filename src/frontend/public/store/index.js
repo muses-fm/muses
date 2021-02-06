eedadebde6212a0aabe1f7b665239f65e7a29a25
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-02-06 21:23:55 +0100

    Add comments to facilitate local development

--- src/frontend/public/store/index.js
+++ src/frontend/public/store/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import artist from 'ic:canisters/artist';
-import curator from 'ic:canisters/curator';
+import artist from 'ic:canisters/artist';  // TO COMMENT OUT WHEN RUNNING DEV SERVER
+import curator from 'ic:canisters/curator';  // TO COMMENT OUT WHEN RUNNING DEV SERVER
 import config from '../config.js'
 
 Vue.use(Vuex)
@@ -54,12 +54,14 @@ export default new Vuex.Store({
     async initializeAppWithDataFromIC({ commit }) {
       commit('SET_STATUS', config.statuses.INITIALIZING)
 
-      const submissions = await artist.getSubmissions()
+      const submissions = await artist.getSubmissions()  // TO COMMENT OUT WHEN RUNNING DEV SERVER
+      // const submissions = [{id: 'test submission'}]
       if (submissions.length > 0) {
         commit('SET_TRACK_SUBMISSIONS', submissions)
       }
 
-      const playlistSubmissions = await curator.getPlaylists()
+      const playlistSubmissions = await curator.getPlaylists()  // TO COMMENT OUT WHEN RUNNING DEV SERVER
+      // const playlistSubmissions = [{id: 'test playlist'}]
       if (playlistSubmissions.length > 0) {
         commit('SET_PLAYLIST_SUBMISSIONS', playlistSubmissions)
       }
@@ -75,24 +77,26 @@ export default new Vuex.Store({
     async submitTrack({ commit }, trackId) {
       commit('TOGGLE_LOADER_ON', 'Storing data...')
       return new Promise((resolve) => {
-        artist.submitTrack(trackId).then(submission => {
+        artist.submitTrack(trackId).then(submission => {  // TO COMMENT OUT WHEN RUNNING DEV SERVER
+          // const submission = 'testtracksubmission'
           commit('SUBMIT_TRACK', submission)
           commit('TOGGLE_LOADER_OFF')
           resolve(submission)
-        })
+        })  // TO COMMENT OUT WHEN RUNNING DEV SERVER
       })
     },
     async submitPlaylist({ commit }, playlistId) {
       commit('TOGGLE_LOADER_ON', 'Storing data...')
       return new Promise((resolve) => {
-        curator.qualifyPlaylist(playlistId).then(submission => {
+        curator.qualifyPlaylist(playlistId).then(submission => {  // TO COMMENT OUT WHEN RUNNING DEV SERVER
+          // const submission = ['testplaylistsubmission']
           // TODO: Find out why returned value is an array with 1 element
           if (submission && submission.length > 0){
             commit('SUBMIT_PLAYLIST', submission[0])
           }
           commit('TOGGLE_LOADER_OFF')
           resolve(submission)
-        })
+        })  // TO COMMENT OUT WHEN RUNNING DEV SERVER
       })
     }
   }
