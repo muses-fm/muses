d11a7168e5c95d477f8e3ffec6e679170d55c2a9
Jose Molina Colmenero <jose@smsdata.es>
2021-02-07 13:08:04 +0100

    Add extra playlists and submissions to fixtures

--- bin/fixtures.sh
+++ bin/fixtures.sh
@@ -8,8 +8,10 @@ usage() {
 }
 
 apply() {
+  dfx canister call curator qualifyPlaylist '("https://open.spotify.com/playlist/5cXbbQOyTdkCu09vEWeEoV?si=G6LYqwImRtuUjvmTKFJ-vw")'
   dfx canister call artist submitTrack '("https://open.spotify.com/track/6HwwPLqfRbYS7peAp1OgFm?si=mYfboeKITt2yIVGxVeFCXw")'
   dfx canister call artist submitTrack '("https://open.spotify.com/track/6UINWt8tEGPYBXEu5VehZs?si=sb0zdWgIS-iK_M0VVmBCxw")'
+  dfx canister call curator reviewSubmission '(1, "This song is awesome! Keep doing your best!", "?1")'
 }
 
 # validate argument length
