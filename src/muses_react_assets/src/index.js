e6036c8807b02f69c5f9df07893eabd82b4b200a
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-10-16 18:15:10 +0200

    Implement demo React app

--- src/muses_react_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { muses_react } from "../../declarations/muses_react";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with muses_react actor, calling the greet method
-  const greeting = await muses_react.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
